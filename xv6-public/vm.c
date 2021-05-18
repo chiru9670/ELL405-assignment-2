5f05a163d4a5e65ca747bcb1a1550755c61dbe80
Anirudh Panigrahi <chiru9670@gmail.com>
2021-05-18 20:53:09 +0530

    Fix stupid mistake in mooveToSwap

--- xv6-public/vm.c
+++ xv6-public/vm.c
@@ -482,11 +482,11 @@ void moveToSwap(pte_t* a, char * pageva){
       p->swapspace_indexes[i] = (uint)pageva;
       *a &= ~PTE_P;
       *a |= PTE_PG;
-      cprintf("before kfree, a = %x, *a=%x, P2V(*a) = %x\n", a, *a, P2V(*a)); //DEBUG
       kfree(P2V(PTE_ADDR(*a)));
+      lcr3(V2P(p->pgdir));
+      return;
     }
   }
-  lcr3(V2P(p->pgdir));
 }
 
 int freeFIFO(){
