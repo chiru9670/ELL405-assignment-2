bb87c79250bc3b5b9b2b8b7dc3b0dbdcdedcb05f
Anirudh Panigrahi <chiru9670@gmail.com>
2021-05-18 17:36:34 +0530

    Modified myMemTest.c

--- xv6-public/myMemTest.c
+++ xv6-public/myMemTest.c
@@ -8,51 +8,62 @@ int
 main(int argc, char *argv[]) {
 
     // allocate 15 pages memory
-    int* ptr[18];
-
-    for(int i=0;i<15;i++) {
-        ptr[i] = (int*) malloc(PGSIZE);
-        for(int j=0;j<PGSIZE/4;j++) {
-            ptr[i][j] = j; 
+    char* ptr[18];
+    const int n = 5;
+    printf(1, "allocate 5 pages memory\n");
+    for(int i=0;i<n;i++) {
+        ptr[i] = sbrk(PGSIZE);
+        for(int j=0;j<PGSIZE/sizeof(char);j+=sizeof(char)) {
+            ptr[i][j] = 'j'; 
         }
     }
 
     processDetailsPrint();
 
-    for(int i=15;i<18;i++) {
-        ptr[i] = (int*) malloc(4096);
-        
-        for(int j=0;j<PGSIZE/4;j++) {
-            ptr[i][j] = j; 
+    // printf(1, "allocating 1 more page...\n");
+    // ptr[12] = sbrk(PGSIZE);
+    // for (int j = 0; j < PGSIZE / sizeof(char); j += sizeof(char))
+    // {
+    //     ptr[12][j] = 'j';
+    // }
+
+    // processDetailsPrint();
+
+    printf(1, "allocate 3 more pages\n");
+    for(int i=n;i<n+3;i++) {
+        ptr[i] = sbrk(PGSIZE);
+        printf(1, "Page %d", i);
+        for(int j=0;j<PGSIZE/sizeof(char);j+=sizeof(char)) {
+            ptr[i][j] = 'j'; 
         }
 
         processDetailsPrint();
     }
 
-    for(int i=0;i<5;i++) {
-        printf(1, "Number ptr[%d][546] = %d", i, ptr[i][546]);
-    }
-    processDetailsPrint();
+    // for(int i=0;i<5;i++) {
+    //     printf(1, "Number ptr[%d][546] = %d", i, ptr[i][546]);
+    // }
+    // processDetailsPrint();
 
-    int a = fork();
+    // int a = fork();
 
-    if(a == 0) {
-        printf(1, "Child is running, pid = %d", getpid());
-        processDetailsPrint();
+    // if(a == 0) {
+    //     printf(1, "Child is running, pid = %d", getpid());
+    //     processDetailsPrint();
 
-        printf(1, "Printing a random value ptr[5][100] = %d", ptr[5][100]);
-        processDetailsPrint();
-        exit();
-    }
-    else {
-        wait();
+    //     printf(1, "Printing a random value ptr[5][100] = %d", ptr[5][100]);
+    //     processDetailsPrint();
+    //     exit();
+    // }
+    // else {
+    //     wait();
 
-        sbrk(-18*PGSIZE);
-        processDetailsPrint();
+    //     sbrk(-18*PGSIZE);
+    //     processDetailsPrint();
 
-        for(int i=0;i<5;i++) ptr[i] = (int*) malloc(PGSIZE);
+    //     for(int i=0;i<5;i++) ptr[i] = (int*) sbrk(PGSIZE);
 
-        processDetailsPrint();
-    }
+    //     processDetailsPrint();
+    // }
 
 }
